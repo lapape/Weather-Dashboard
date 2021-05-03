ea88042b24f003ae077f1b83a8655ad6183c15f4
lapape <lapape07@gmail.com>
2021-05-03 17:47:15 -0600

    set up local storage for user inputs and displays searches in a list

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,33 @@ var searchedCities = JSON.parse(localStorage.getItem("cities")) || [];
 var userInput = $(".userInput");
 var citiesUl = $("#city-list");
 
-$(".searchBtn").on("click", function () {
-  searchedCities.push(userInput.val());
-  localStorage.setItem("cities", JSON.stringify(searchedCities));
+for (var i = 0; i < searchedCities.length; i++) {
+  var newLi = document.createElement("LI");
 
-  for (var i = 0; i < searchedCities.length; i++) {
-    var newLi = document.createElement("LI");
-    newLi.textContent = searchedCities[i];
-    citiesUl.appendChild(newLi);
+  newLi.textContent = searchedCities[i];
+  citiesUl.append(newLi);
+}
+$(".searchBtn").on("click", function () {
+  if (userInput.val() === "") {
+    alert("Error, must enter a city!");
+  } else {
+    if (searchedCities.includes(userInput.val())) {
+      alert("City already searched!");
+    } else {
+      $("#city-list").empty();
+      searchedCities.push(userInput.val());
+      localStorage.setItem("cities", JSON.stringify(searchedCities));
+      for (var i = 0; i < searchedCities.length; i++) {
+        var newLi = document.createElement("LI");
+
+        newLi.textContent = searchedCities[i];
+        citiesUl.append(newLi);
+      }
+    }
   }
 });
 
 $(".clearBtn").on("click", function () {
-  searchedCities = [];
+  localStorage.clear();
+  location.reload();
 });
