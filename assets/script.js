1b209c68943e6ddfa2c315043039e4448793a5bd
lapape <lapape07@gmail.com>
2021-05-02 23:51:36 -0600

    set up local storage for previously searched cities

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,18 @@
 var searchedCities = JSON.parse(localStorage.getItem("cities")) || [];
+var userInput = $(".userInput");
+var citiesUl = $("#city-list");
 
 $(".searchBtn").on("click", function () {
-  console.log("hi");
+  searchedCities.push(userInput.val());
+  localStorage.setItem("cities", JSON.stringify(searchedCities));
+
+  for (var i = 0; i < searchedCities.length; i++) {
+    var newLi = document.createElement("LI");
+    newLi.textContent = searchedCities[i];
+    citiesUl.appendChild(newLi);
+  }
+});
+
+$(".clearBtn").on("click", function () {
+  searchedCities = [];
 });
